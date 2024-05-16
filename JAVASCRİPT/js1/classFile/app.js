30d18f39247897c5f5ea3dde1b230d576596d8d6
ErgunSekercan <ergunsekercan@gmail.com>
2024-05-16 15:54:22 +0300

    Kayseri_2

---
+++ JAVASCRİPT/js1/classFile/app.js
@@ -0,0 +1,64 @@
+// console.log(window.document)
+
+//! declarasyon
+
+// var , let , const
+
+var firstName = "gökhan"; // şuan kullanımda değil eski projelerde görürsünüz.
+
+let lastName;
+
+const birthDay = 1980
+
+lastName = "mahmut"
+
+let lastname = "ihsan"
+
+//! output
+
+
+//? console
+console.log(firstName)
+console.log("firstName")
+console.log(1980)
+
+console.clear()
+
+// ******************
+
+//? açılır pencere
+
+// alert("internet sitesine giriş yaptınız")
+// console.log(alert("internet sitesine giriş yaptınız"))
+
+
+// console.log(confirm("ürün sepetten kaldırılacak emin misiniz"))
+// confirm("ürün sepetten kaldırılacak emin misiniz")
+
+// let age = prompt("kac yasındasın")
+
+// console.log(age)
+
+
+//? ana ekran
+
+let deneme = document.querySelector("h1")
+
+console.log(deneme)
+
+deneme.innerText = "javascript"
+
+deneme.style.color = "red"
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JAVASCRİPT/js1/classFile/dataType.js
@@ -0,0 +1,38 @@
+//! data type
+
+// primitive
+
+// string => klavyeden girilebilen herşey
+
+let str = " ahmet 27 yaşında mı ? "
+
+console.log(str)
+console.log(typeof str)
+
+// number
+
+let num = 3453
+
+console.log(typeof num , num)
+
+console.log(0,999999999999999999999)
+
+// BigInt
+
+console.log(0,999999999999999999999n)
+
+// boolean
+
+let control = 5 > 6
+
+console.log(control)
+console.log(typeof control)
+
+// null
+
+
+
+
+
+
+// non-primitive , referans model, object
